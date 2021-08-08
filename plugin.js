7295e085cf6560ba0b7bed4d2f03f386674c3667
Aniruddha-Deb <aniruddha.deb.2002@gmail.com>
2021-08-08 12:31:16 +0530

    Better listener

--- plugin.js
+++ plugin.js
@@ -1,1 +1,14 @@
-$( "div[id^='ad']" ).remove()
+console.log("Loaded ad remover plugin")
+var config = { childList: true, subtree: true };
+var feed = $("div[class='imus-student-wrapper']")[0];
+
+var callback = function(mutationsList) {
+	var ads = $( "div[id^='ad']" )
+	if (ads.length !== 0) {
+		ads.remove();
+		console.log('Removed ads');
+	}
+};
+
+var observer = new MutationObserver(callback);
+observer.observe(feed, config);
